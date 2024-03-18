c8ddf600c88b079fb19db844d16d8245e04d0b3b
Harshit-1874 <93092497+Harshit-1874@users.noreply.github.com>
2024-03-18 11:45:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,58 +1,69 @@
-const express = require("express");
+const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.post("/bfhl", async (req, res) => {
-  try {
-    const data = req.body;
-    const s = true;
-    const user = "harshit_aggarwal_18072004";
-    const email = "harshit1084.be21@chitkarauniversity.edu.in";
-    const roll = "2111981084";
-    const numbers = [];
-    const alphabets = [];
-    //for loop to traverse through data
-    const regex = /^[a-zA-Z]+$/;
-    for (let i = 0; i < data.length; i++) {
-      //check if the string in data is numbers or alphabets
-      if (regex.test(data[i])) {
-        alphabets.push(data[i].toUppercase());
-      } else {
-        numbers.push(data[i]);
-      }
-    }
-    const odd_numbers = [];
-    const even_numbers = [];
 
-    //for loop to traverse through numbers
-    for (let i = 0; i < numbers.length; i++) {
-      //convert the string of number into integer and check parity
-      const d = parseInt(numbers[i]);
-      if (d % 2 == 0) {
-        even_numbers.push(numbers[i]);
-      } else {
-        odd_numbers.push(numbers[i]);
-      }
-    }
+app.use(bodyParser.json());
 
-    res.json({
-      is_success: s,
-      user_id: user,
-      email: email,
-      roll_number: roll,
-      odd_numbers: odd_numbers,
-      even_numbers: even_numbers,
-      alphabets: alphabets,
-    });
-  } catch (e) {
-    const s = false;
-    res.json({
-      is_success: s,
-      error: e,
-    });
-  }
+
+app.get('/',(req,res)=>{
+    res.write("Hello World");
+    res.end();
+})
+
+let response={
+
+};
+
+app.get('/bfhl',(req,res)=>{
+    let list=JSON.stringify(response);
+    res.write(list);
+    res.end();
+})
+
+app.post('/bfhl', (req, res) => {
+
+        const data = req.body.data;
+
+
+        const user_id = "harshit_18072004";
+
+
+        const email = "harshit1084.be21@chitkarauniversity.edu.in";
+        const roll_number = "2111981084";
+        const odd_numbers = [];
+        const even_numbers = [];
+        const alphabets = [];
+
+
+        data.forEach(element => {
+            if (typeof element === 'string' && isNaN(element)) {
+                alphabets.push(element.toUpperCase());
+            } else if (!isNaN(element)) {
+                if (parseInt(element) % 2 === 0) {
+                    even_numbers.push(element);
+                } else {
+                    odd_numbers.push(element);
+                }
+            }
+        });
+
+
+        response = {
+            is_success: true,
+            user_id: user_id,
+            email: email,
+            roll_number: roll_number,
+            odd_numbers: odd_numbers,
+            even_numbers: even_numbers,
+            alphabets: alphabets
+        };
+        res.json(response);
 });
 
-app.listen(3000, () => {
-  console.log("server is running on port 3000");
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
